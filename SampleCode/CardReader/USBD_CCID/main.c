e258124cdb1772e2cefbbdefd7af8d8a427d451f
Kenny Hsieh <CHHsieh3@nuvoton.com>
2020-02-17 13:33:49 +0800

    Add card reader sample code: USBD_CCID

---
+++ SampleCode/CardReader/USBD_CCID/main.c
@@ -0,0 +1,227 @@
+/****************************************************************************
+ * @file     main.c
+ * @version  V2.0
+ * @brief    USB CCID smart card reader sample code.
+ * @note
+ * Copyright (C) 2020 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "ccid.h"
+#include "ccid_if.h"
+#include "sclib.h"
+
+#define CRYSTAL_LESS    1 /* CRYSTAL_LESS must be 1 if USB clock source is HIRC */
+#define TRIM_INIT           (SYS_BASE+0x118)
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global variables                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+#define INT_BUFFER_SIZE     64    /* Interrupt message buffer size */
+#define BULK_BUFFER_SIZE    512   /* bulk message buffer size */
+
+uint8_t UsbIntMessageBuffer[INT_BUFFER_SIZE];
+uint8_t UsbMessageBuffer[BULK_BUFFER_SIZE];
+
+uint8_t volatile gu8IsDeviceReady;
+uint8_t volatile gu8AbortRequestFlag;
+uint8_t volatile gu8IsBulkOutReady;
+uint8_t volatile gu8IsBulkInReady;
+
+uint8_t *pu8IntInBuf;
+uint8_t *pUsbMessageBuffer;
+uint32_t volatile u32BulkSize;
+
+int32_t volatile gi32UsbdMessageLength;
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* The interrupt services routine of smartcard port 0                                                      */
+/*---------------------------------------------------------------------------------------------------------*/
+void SC0_IRQHandler(void)
+{
+    /* Please don't remove any of the function calls below */
+    if(SCLIB_CheckCDEvent(0))
+    {
+        RDR_to_PC_NotifySlotChange();
+        USBD_MemCopy((uint8_t *)(USBD_BUF_BASE + USBD_GET_EP_BUF_ADDR(EP4)), pu8IntInBuf, 2);
+        USBD_SET_PAYLOAD_LEN(EP4, 2);
+        return; // Card insert/remove event occurred, no need to check other event...
+    }
+
+    SCLIB_CheckTimeOutEvent(0);
+    SCLIB_CheckTxRxEvent(0);
+    SCLIB_CheckErrorEvent(0);
+
+    return;
+}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable HIRC clock (Internal RC 22.1184MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Wait for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+   
+#if (CRYSTAL_LESS)
+    /* Switch HCLK clock source to Internal HIRC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Select module clock source */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USBDSEL_HIRC, CLK_CLKDIV0_USB(1));
+#else
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_48MHZ);
+
+    /* Select module clock source */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USBDSEL_PLL, CLK_CLKDIV0_USB(2));
+
+    /* Disable digital input path of analog pin XT1_OUT to prevent leakage */
+    GPIO_DISABLE_DIGITAL_PATH(PF, BIT2 | BIT3);
+#endif    
+    
+    SystemCoreClockUpdate();
+    
+    /* Select module clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(SC0_MODULE, CLK_CLKSEL3_SC0SEL_HIRC, CLK_CLKDIV1_SC0(12));
+
+    /* Enable module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(SC0_MODULE);
+    CLK_EnableModuleClock(USBD_MODULE);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set PA multi-function pins for UART0 RXD and TXD */
+    Uart0DefaultMPF();
+
+    /* Set PB.2 ~ PB.5 and PA.4 for SC0 interface */
+    SYS->GPB_MFPL &= ~(SYS_GPB_MFPL_PB2MFP_Msk | SYS_GPB_MFPL_PB3MFP_Msk
+                       | SYS_GPB_MFPL_PB4MFP_Msk | SYS_GPB_MFPL_PB5MFP_Msk);
+    SYS->GPB_MFPL |= (SYS_GPB_MFPL_PB2MFP_SC0_PWR | SYS_GPB_MFPL_PB3MFP_SC0_RST
+                      | SYS_GPB_MFPL_PB4MFP_SC0_DAT | SYS_GPB_MFPL_PB5MFP_SC0_CLK);
+    
+    SYS->GPA_MFPL &= ~SYS_GPA_MFPL_PA4MFP_Msk;
+    SYS->GPA_MFPL |= SYS_GPA_MFPL_PA4MFP_SC0_nCD;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Init UART                                                                                               */
+/*---------------------------------------------------------------------------------------------------------*/
+void UART0_Init()
+{
+    /* Reset IP */
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 Baudrate */
+    UART_Open(UART0, 115200);
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* MAIN function                                                                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+int main(void)
+{
+    
+#if CRYSTAL_LESS
+    uint32_t u32TrimInit;
+#endif    
+    
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Init UART0 for printf */
+    UART0_Init();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+    printf("+-----------------------------------------------------+\n");
+    printf("|    NuMicro USB CCID SmartCard Reader Sample Code    |\n");
+    printf("+-----------------------------------------------------+\n");
+
+    /* Open smartcard interface 0. CD pin state low indicates card insert and PWR pin low raise VCC pin to card */
+    SC_Open(SC0, SC_PIN_STATE_LOW, SC_PIN_STATE_HIGH);
+    NVIC_EnableIRQ(SC0_IRQn);
+
+    SC0->INTEN = SC_INTEN_CDIEN_Msk;
+
+    USBD_Open(&gsInfo, CCID_ClassRequest, NULL);
+
+    /* Endpoint configuration */
+    CCID_Init();
+
+    /* Set priority is a must under current architecture. Otherwise smartcard interrupt will be blocked by USBD interrupt */
+    NVIC_SetPriority(USBD_IRQn, (1 << __NVIC_PRIO_BITS) - 2);
+
+    NVIC_EnableIRQ(USBD_IRQn);
+    USBD_Start();
+
+#if CRYSTAL_LESS
+    /* Backup default trim value */
+    u32TrimInit = M32(TRIM_INIT);
+
+    /* Clear SOF */
+    USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+#endif
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+
+        /* Start USB trim function if it is not enabled. */
+        if ((SYS->HIRCTRIMCTL & SYS_HIRCTRIMCTL_FREQSEL_Msk) != 0x1)
+        {
+            /* Start USB trim only when USB signal arrived */
+            if (USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+                    
+                /* 
+                    USB clock trim function: 
+                    HIRC Trimming with boundary function enhances robustility 
+                    and keeps HIRC in right frequency while receiving unstable USB signal 
+                */
+                SYS->HIRCTRIMCTL =  ( 0x1 << SYS_HIRCTRIMCTL_REFCKSEL_Pos )
+                                  | ( 0x1 << SYS_HIRCTRIMCTL_FREQSEL_Pos  )
+                                  | ( 0x0 << SYS_HIRCTRIMCTL_LOOPSEL_Pos  )
+                                  | ( 0x1 << SYS_HIRCTRIMCTL_BOUNDEN_Pos  )
+                                  | ( 10  << SYS_HIRCTRIMCTL_BOUNDARY_Pos );
+            }
+        }
+
+        /* Disable USB Trim when any error found */
+        if (SYS->HIRCTRIMSTS & (SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable USB clock trim function */
+            SYS->HIRCTRIMCTL = 0;
+
+            /* Clear trim error flags */
+            SYS->HIRCTRIMSTS = SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk;
+
+            /* Clear SOF */
+            USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+        }
+
+#endif
+    }
+}
