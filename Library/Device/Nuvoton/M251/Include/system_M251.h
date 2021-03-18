4d11ee0cda6856435996053ed24ecbb836bf7037
Kenny Hsieh <CHHsieh3@nuvoton.com>
2021-03-18 18:50:04 +0800

    Added condition to define HXT/LXT

--- Library/Device/Nuvoton/M251/Include/system_M251.h
+++ Library/Device/Nuvoton/M251/Include/system_M251.h
@@ -19,13 +19,19 @@ extern "C" {
 /*----------------------------------------------------------------------------
   Define clocks
  *----------------------------------------------------------------------------*/
-#define __HXT       (12000000UL)              /*!< External Crystal Clock Frequency */
+#ifndef __HXT
+#define __HXT       (12000000UL)              /*!< External Crystal Clock Frequency           */
+#endif /* !defined(__HXT) */
+
+#ifndef __LXT
 #define __LXT       (32768UL)                 /*!< External Crystal Clock Frequency 32.768KHz */
-#define __LIRC      (38400UL)                 /*!< Internal 38.4KHz RC Oscillator Frequency */
-#define __MIRC      (4032000UL)               /*!< Internal 4.032M RC Oscillator Frequency */
-#define __HIRC      (48000000UL)              /*!< Internal 48M RC Oscillator Frequency */
-#define __HSI       (48000000UL)              /*!< PLL default output is 48MHz */
-#define __SYS_OSC_CLK     (    ___HSI)        /*!< Main oscillator frequency */
+#endif /* !defined(__LXT) */
+
+#define __LIRC      (38400UL)                 /*!< Internal 38.4KHz RC Oscillator Frequency   */
+#define __MIRC      (4032000UL)               /*!< Internal 4.032M RC Oscillator Frequency    */
+#define __HIRC      (48000000UL)              /*!< Internal 48M RC Oscillator Frequency       */
+#define __HSI       (48000000UL)              /*!< PLL default output is 48MHz                */
+#define __SYS_OSC_CLK     (    ___HSI)        /*!< Main oscillator frequency                  */
 
 #define __SYSTEM_CLOCK    (1UL*__HXT)
 
